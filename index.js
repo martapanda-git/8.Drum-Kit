fd5ef0a05af9b4191b7cc48f37caf4feb62bdc43
martapanda <marta.urbowska@gmail.com>
2021-04-21 18:41:41 +0200

    Added alert in js to the all buttons

---
+++ index.js
@@ -0,0 +1,21 @@
+// alert("Hello there!");
+// document.querySelector("button").addEventListener("click", handleClick);
+//
+// function handleClick(){
+//   alert("I got clicked!");
+// }
+
+
+// document.querySelector("button").addEventListener("click", function (){
+//   alert("I got clicked!");
+// }
+// );
+
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (i = 0; i<numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    alert("I got clicked");
+  });
+}
